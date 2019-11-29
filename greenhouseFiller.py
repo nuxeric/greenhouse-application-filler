8cf8906cb8ad3c5116387189d35a96a506cdbcfe
Eric Son <nuxeric@hotmail.com>
2019-11-29 07:45:59 -0800

    changed config file to be a dictionary, added in pyyaml as req

--- greenhouseFiller.py
+++ greenhouseFiller.py
@@ -2,7 +2,7 @@ import sys
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
+import yaml
 
 def get_all_field_divs(soup_element):
     return soup_element.find_all(class_="field")
@@ -10,15 +10,21 @@ def get_all_field_divs(soup_element):
 def get_form_with_id(id_name, soup_element):
     return soup_element.form.find(id=id_name)
 
+
+
 if len(sys.argv) != 2:
     print("To run this script you must give it a url")
     sys.exit()
-
 url = sys.argv[1]
+
+with open("person_config.yml", "r") as file:
+    data = yaml.load(file.read(), Loader=yaml.FullLoader)
+    print(data)
+    print(type(data))
+
 
 r = requests.get(url)
 data = r.text
-
 soup = BeautifulSoup(data, "html.parser")
 soup_application = soup.body.find(id="main").find(id="application")
 
